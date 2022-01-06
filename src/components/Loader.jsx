6f36a01009a38675e77b83a55f96b44eeb09e7f5
Abolfazl Amiri <aa.smpro@gmail.com>
2022-01-06 21:37:40 +0330

    fix: make it even better

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,9 +6,9 @@ export const Loader = ({ is_loaded }) => {
   return is_loaded ? null : (
     <div className="print:hidden fixed min-h-screen min-w-full z-50 flex flex-col justify-center items-center bg-white">
       <div
-        className={`absolute min-w-[50px] min-h-[50px] rounded-full bg-${stg.theme_color}-300 animate-ping`}></div>
+        className={`absolute min-w-[200px] min-h-[200px] rounded-full bg-${stg.theme_color}-300 animate-ping blur`}></div>
       <div
-        className={`min-w-[30px] min-h-[30px] rounded-full bg-${stg.theme_color}-300 animate-pulse`}></div>
+        className={`min-w-[50px] min-h-[50px] rounded-full bg-${stg.theme_color}-300 animate-pulse opacity-20`}></div>
     </div>
   );
 };
