212f11b34f18d727417a512e61a9f0570b3122b4
Abolfazl Amiri <aa.smpro@gmail.com>
2022-06-29 15:24:53 +0430

    fix: logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -10,11 +10,11 @@ export const Logo = () => {
         className={`group w-[35px] h-[35px] max-w-[35px] max-h-[35px] min-w-[35px] min-h-[35px] bg-${stg.theme_color}-300 rounded-full transition duration-300 hover:ring-2 hover:ring-${stg.theme_color}-300 hover:bg-white`}>
         <div className="relative">
           <div
-            className={`absolute font-extrabold text-3xl left-[4px] text-white transition duration-300 group-hover:text-${stg.theme_color}-900`}>
+            className={`absolute font-extrabold text-3xl left-[3px] text-white transition duration-300 group-hover:text-${stg.theme_color}-900`}>
             F
           </div>
           <div
-            className={`absolute font-extrabold text-3xl left-[13px] text-${stg.theme_color}-900 transition duration-300 group-hover:text-${stg.theme_color}-300`}>
+            className={`absolute font-extrabold text-3xl left-[11px] text-${stg.theme_color}-900 transition duration-300 group-hover:text-${stg.theme_color}-300`}>
             A
           </div>
         </div>
